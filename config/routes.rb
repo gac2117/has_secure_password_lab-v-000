c5a5132c1d1aaaccf8266bfe7eb45ec21c1f9161
Grace Choi <oiograceoio@gmail.com>
2019-05-15 16:07:20 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  get '/signup' => 'users#new'
-  post '/signup' => 'users#create'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  post '/logout' => 'sessions#destroy'
+
+  resources :users, only: [:new, :create]
+
+  root 'welcome#home'
 end
